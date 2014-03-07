31b1b0c90269628d7f2c5ad4381d1c90c32aa941
Myiosus <yeskwang@yahoo.com>
2014-03-07 08:37:45 +0800

    Updated database with new password (md5_salt key)

--- toto_result.sql
+++ toto_result.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.1
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 23, 2014 at 02:57 PM
--- Server version: 5.6.11
--- PHP Version: 5.5.3
+-- Host: localhost
+-- Generation Time: Mar 07, 2014 at 12:32 AM
+-- Server version: 5.6.12-log
+-- PHP Version: 5.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -30,12 +30,33 @@ USE `toto_result`;
 
 CREATE TABLE IF NOT EXISTS `result` (
   `resultid` int(6) NOT NULL AUTO_INCREMENT,
-  `resultnumber` int(4) NOT NULL,
+  `resultnumber` varchar(4) NOT NULL,
   `resultdate` date NOT NULL,
-  `prize` int(2) NOT NULL,
+  `prize` varchar(2) NOT NULL,
   `vendor` varchar(3) NOT NULL,
   PRIMARY KEY (`resultid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+
+--
+-- Dumping data for table `result`
+--
+
+INSERT INTO `result` (`resultid`, `resultnumber`, `resultdate`, `prize`, `vendor`) VALUES
+(1, '1234', '2014-03-05', '01', '01'),
+(2, '0851', '2014-03-05', '02', '01'),
+(3, '5801', '2014-03-05', '03', '01'),
+(4, '6890', '2014-03-05', '10', '01'),
+(5, '3493', '2014-03-05', '11', '01'),
+(6, '7803', '2014-03-05', '01', '02'),
+(7, '4687', '2014-03-05', '02', '02'),
+(8, '3792', '2014-03-05', '03', '02'),
+(9, '3890', '2014-03-05', '10', '02'),
+(10, '4590', '2014-03-05', '11', '02'),
+(11, '3435', '2014-03-05', '01', '03'),
+(12, '3293', '2014-03-05', '02', '03'),
+(13, '9766', '2014-03-05', '03', '03'),
+(14, '8392', '2014-03-05', '10', '03'),
+(15, '4904', '2014-03-05', '11', '03');
 
 -- --------------------------------------------------------
 
@@ -45,8 +66,17 @@ CREATE TABLE IF NOT EXISTS `result` (
 
 CREATE TABLE IF NOT EXISTS `user` (
   `userid` varchar(16) NOT NULL,
-  `password` varchar(16) NOT NULL
+  `password` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userid`, `password`) VALUES
+('Carbon', '6b3f244f179659616aadfc6d193103c8'),
+('Jy', '89c868c8ff892bddf6e21e6883e69159'),
+('Myiosus', '7db6e35659645f937556524ee14d3a83');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
